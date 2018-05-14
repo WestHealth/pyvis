bb09e48d08f76ff2baac9fc3d060f70cde4c92f8
Giancarlo Perrone <gperrone@westhealth.org>
2018-05-14 16:39:58 -0700

    added some logic to account for seeting font color

--- pyvis/node.py
+++ pyvis/node.py
@@ -1,7 +1,9 @@
 class Node(object):
 
-    def __init__(self, n_id, shape, label, **opts):
+    def __init__(self, n_id, shape, label, font_color=False, **opts):
         self.options = opts
         self.options["id"] = n_id
         self.options["label"] = label
         self.options["shape"] = shape
+        if font_color:
+            self.options["font"] = dict(color=font_color)
