4d159a595a4ae392415158fe3d661964f516b61b
Giancarlo Perrone <gperrone@dev.whidsc.net>
2019-03-11 16:57:31 -0700

    added layout structure to represent visjs layout obj

--- pyvis/options.py
+++ pyvis/options.py
@@ -75,14 +75,83 @@ class Configure(object):
         return self.__dict__[item]
 
 
+class Layout(object):
+    """
+    Acts as the camera that looks on the canvas.
+    Does the animation, zooming and focusing.
+    """
+    
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __init__(self, randomSeed=None, improvedLayout=True):
+            if not randomSeed:
+                self.randomSeed = 0
+            else:
+                self.radnomSeed = randomSeed
+            self.improvedLayout = improvedLayout
+            self.hierarchical = self.Hierarchical(enabled=True)
+    
+    def set_separation(self, distance):
+        """
+        The distance between the different levels.
+        """
+        self.hierarchical.levelSeparation = distance
+    
+    def set_tree_spacing(self, distance):
+        """
+        Distance between different trees (independent networks). This is
+        only for the initial layout. If you enable physics, the repulsion
+        model will denote the distance between the trees.
+        """
+        self.hierarchical.treeSpacing = distance
+
+    def set_edge_minimization(self, status):
+        """
+        Method for reducing whitespace. Can be used alone or together with
+        block shifting. Enabling block shifting will usually speed up the
+        layout process. Each node will try to move along its free axis to
+        reduce the total length of it's edges. This is mainly for the
+        initial layout. If you enable physics, they layout will be determined
+        by the physics. This will greatly speed up the stabilization time
+        """
+        self.hierarchical.edgeMinimization = status
+
+    class Hierarchical(object):
+
+        def __getitem__(self, item):
+            return self.__dict__[item]
+
+        def __init__(self,
+                    enabled=False,
+                    levelSeparation=150,
+                    treeSpacing=200,
+                    blockShifting=True,
+                    edgeMinimization=True,
+                    parentCentralization=True,
+                    sortMethod='hubsize'):
+
+            self.enabled = enabled
+            self.levelSeparation = levelSeparation
+            self.treeSpacing = treeSpacing
+            self.blockShifting = blockShifting
+            self.edgeMinimization = edgeMinimization
+            self.parentCentralization = parentCentralization
+            self.sortMethod = sortMethod
+
+    
 
 class Options(object):
 
     def __repr__(self):
         return str(self.__dict__)
 
-    def __init__(self):
-        # self.layout = Layout()
+    def __getitem__(self, item):
+        return self.__dict__[item]
+
+    def __init__(self, layout=None):
+        if layout:
+            self.layout = Layout()
         self.interaction = Interaction()
         self.configure = Configure()
         self.physics = Physics()
