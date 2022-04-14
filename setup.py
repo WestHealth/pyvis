ea5a9e1994b46854e32cc5f1eead9b6516d830cb
jhunpingco <jhunpingco@westhealth.org>
2022-04-14 09:02:52 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 exec(open('pyvis/_version.py').read())
 setup(
     name="pyvis",
     version=__version__,
     description="A Python network graph visualization library",
-    long_description='Pyvis is a wrapper around visjs, a JavaScript network graph visualization library.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/WestHealth/pyvis",
     author="Jose Unpingco",
     author_email="datascience@westhealth.org",
@@ -17,5 +21,6 @@ setup(
         "networkx >= 1.11",
         "ipython >= 5.3.0",
         "jsonpickle >= 1.4.1"
-    ]
+    ],
+    python_requires=">3.6",
 )
